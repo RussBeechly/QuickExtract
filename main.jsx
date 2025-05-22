ac9601262f8f0ac581deaf585a08959030eb7dcb
RussBeechly <russ.beechly@samlanders.com>
2025-05-22 15:44:50 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import QuickExtract from "./QuickExtract";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID_HERE">
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <QuickExtract />
   </GoogleOAuthProvider>
 );

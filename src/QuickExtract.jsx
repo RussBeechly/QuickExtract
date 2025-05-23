ce556b7e88bb3dd9d41d6de258a077e2290ffd3c
Russ Beechly <russ.beechly@samlanders.com>
2025-05-23 10:53:21 -0400

    Remove unused Input import

--- src/QuickExtract.jsx
+++ src/QuickExtract.jsx
@@ -1,7 +1,6 @@
 
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
-import { Input } from "../components/ui/input";
 import { Button } from "../components/ui/button";
 import { Card, CardContent } from "../components/ui/card";
 

fbefcec2d0da0ac7f8b0317691a1aa73e555e424
Russ Beechly <russ.beechly@samlanders.com>
2025-05-23 10:27:17 -0400

    Fix component import path

--- quickextract-app/src/QuickExtract.jsx
+++ quickextract-app/src/QuickExtract.jsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
-import { Input } from "../components/ui/input";
+import { Input } from "../components/ui/Input";
 import { Button } from "../components/ui/button";
 import { Card, CardContent } from "../components/ui/card";
 

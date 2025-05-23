8124160819bfa112707bb1f40d19c90d18298920
Russ Beechly <russ.beechly@samlanders.com>
2025-05-23 11:11:07 -0400

    Remove unused Button import

--- src/QuickExtract.jsx
+++ src/QuickExtract.jsx
@@ -1,7 +1,6 @@
 
 import React, { useState } from "react";
 import * as XLSX from "xlsx";
-import { Button } from "../components/ui/button";
 import { Card, CardContent } from "../components/ui/card";
 
 export default function QuickExtract() {
